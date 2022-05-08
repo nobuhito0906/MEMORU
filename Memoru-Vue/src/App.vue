d9a6032c6c3c23e15ad1a5bbc5519c9645d5564d
nobuhito0906 <77920457+nobuhito0906@users.noreply.github.com>
2022-05-09 00:35:30 +0900

    Jsonデータ外だし
    tsconfig.json作成

--- Memoru-Vue/src/App.vue
+++ Memoru-Vue/src/App.vue
@@ -7,46 +7,15 @@
 </template>
 
 <script lang="ts">
+import columns from './assets/TodoColumns.json'
+import rows from './assets/Todo.json'
 export default {
   name: "Todos",
   data() {
     return {
       userId: "",
-      columns: [
-        {
-          label: "ID",
-          field: "id",
-          type: "number",
-        },
-        {
-          label: "メモ",
-          field: "memo",
-        },
-        {
-          label: "備考",
-          field: "remarks",
-        },
-        {
-          label: "ユーザID",
-          field: "userId",
-        },
-        {
-          label: "行動日",
-          field: "actionDate",
-          type: "date",
-          dateInputFormat: "yyyy-MM-dd'T'HH:mm:ss.SSSX",
-          dateOutputFormat: "MMM do yy",
-        },
-      ],
-      rows: [
-        {
-          id: 111,
-          memo: "メモ1",
-          remarks: "備考1",
-          userId: "ABD",
-          actionDate: new Date(),
-        },
-      ],
+      columns: columns,
+      rows: rows
     };
   },
 };
